d2ca6b47e00bd827db0ce361d5d1e85a421a1472
hamzah <haldolaay@gmail.com>
2026-04-18 17:56:42 -0700

    changing approach

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,24 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // GitHub project Pages URL: https://<user>.github.io/<REPO>/
+// // Must match the repository name (change if your repo is not "GiovannyPortfolio").
+// const GH_PAGES_BASE = '/GiovannyPortfolio/'
+
+
+// // https://vite.dev/config/
+// export default defineConfig(({ mode }) => ({
+//    base: mode === 'production' ? GH_PAGES_BASE : '/',
+//   // base: '/',
+//   plugins: [react()],
+//   /** So `import '…/file.fbx?url'` works from `src/assets` */
+//   assetsInclude: ['**/*.fbx', '**/*.FBX', '**/*.pdf'],
+// }))
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// GitHub project Pages URL: https://<user>.github.io/<REPO>/
-// Must match the repository name (change if your repo is not "GiovannyPortfolio").
-const GH_PAGES_BASE = '/GiovannyPortfolio/'
-
-
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-   base: mode === 'production' ? GH_PAGES_BASE : '/',
-  // base: '/',
+  base: mode === 'production' ? (process.env.CUSTOM_DOMAIN ? '/' : '/GiovannyPortfolio/') : '/',
   plugins: [react()],
-  /** So `import '…/file.fbx?url'` works from `src/assets` */
   assetsInclude: ['**/*.fbx', '**/*.FBX', '**/*.pdf'],
 }))
